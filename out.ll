dd25bcd1a750fde5c1d7d678e9e9ecc4bfe8c3e0
Ulysse Carion <ulysse@ulysse.io>
2016-07-04 11:15:16 -0700

    Finish off `if`

--- out.ll
+++ out.ll
@@ -2,5 +2,49 @@
 
 define i64 @main() {
 entry:
-  ret i64 42
+  %a = alloca i64
+  %b = alloca i64
+  %c = alloca i64
+  store i64 1, i64* %a
+  store i64 0, i64* %b
+  %a1 = load i64, i64* %a
+  %is_nonzero = icmp ne i64 %a1, 0
+  br i1 %is_nonzero, label %entry2, label %entry3
+
+entry2:                                           ; preds = %entry
+  %b5 = load i64, i64* %b
+  %is_nonzero6 = icmp ne i64 %b5, 0
+  br i1 %is_nonzero6, label %entry7, label %entry8
+
+entry3:                                           ; preds = %entry
+  %b10 = load i64, i64* %b
+  %is_nonzero11 = icmp ne i64 %b10, 0
+  br i1 %is_nonzero11, label %entry12, label %entry13
+
+entry4:                                           ; preds = %entry14, %entry9
+  %iftmp16 = phi i64 [ %iftmp, %entry9 ], [ %iftmp15, %entry14 ]
+  store i64 %iftmp16, i64* %c
+  %c17 = load i64, i64* %c
+  %addtmp = add i64 %c17, 2
+  ret i64 %addtmp
+
+entry7:                                           ; preds = %entry2
+  br label %entry9
+
+entry8:                                           ; preds = %entry2
+  br label %entry9
+
+entry9:                                           ; preds = %entry8, %entry7
+  %iftmp = phi i64 [ 11, %entry7 ], [ 40, %entry8 ]
+  br label %entry4
+
+entry12:                                          ; preds = %entry3
+  br label %entry14
+
+entry13:                                          ; preds = %entry3
+  br label %entry14
+
+entry14:                                          ; preds = %entry13, %entry12
+  %iftmp15 = phi i64 [ 10, %entry12 ], [ 20, %entry13 ]
+  br label %entry4
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::fs::File;
 use std::ptr;
 use std::io::Read;
 use std::ffi::CString;
+use std::collections::{HashMap, HashSet};
 
 use llvm::prelude::*;
 
@@ -22,13 +23,15 @@ fn main() {
     }
 }
 
-// Some sort of "expression", in our case it's just arithmetic
 pub enum Expr {
+    Literal(String),
+    Ref(String),
+    Assign(String, Box<Expr>),
     Add(Box<Expr>, Box<Expr>),
     Sub(Box<Expr>, Box<Expr>),
     Mul(Box<Expr>, Box<Expr>),
     Div(Box<Expr>, Box<Expr>),
-    Literal(String),
+    If(Box<Expr>, Vec<Expr>, Vec<Expr>),
 }
 
 // `product` and `sum` are that way to get operator precedence
@@ -36,11 +39,21 @@ peg! parser(r#"
     use super::Expr;
 
     #[pub]
-    program -> Expr
-        = e:expression "\n" { e }
+    program -> Vec<Expr>
+        = statements
+
+    statements -> Vec<Expr>
+        = e:(expression ** ("\n" _)) "\n" { e }
 
     expression -> Expr
-        = sum
+        = if_expression
+        / i:identifier _ "=" _ s:expression { Expr::Assign(i, Box::new(s)) }
+        / sum
+
+    if_expression -> Expr
+        = "if" _ e:expression _ "{\n" _ then_body:statements _ "}" _ "else" _ "{\n" _ else_body:statements _ "}" {
+            Expr::If(Box::new(e), then_body, else_body)
+        }
 
     sum -> Expr
         = a:product _ "+" _ b:sum { Expr::Add(Box::new(a), Box::new(b)) }
@@ -48,17 +61,24 @@ peg! parser(r#"
         / product
 
     product -> Expr
-        = a:int_literal _ "*" _ b:product { Expr::Mul(Box::new(a), Box::new(b)) }
-        / a:int_literal _ "/" _ b:product { Expr::Div(Box::new(a), Box::new(b)) }
+        = a:ref_or_literal _ "*" _ b:product { Expr::Mul(Box::new(a), Box::new(b)) }
+        / a:ref_or_literal _ "/" _ b:product { Expr::Div(Box::new(a), Box::new(b)) }
+        / ref_or_literal
+
+    ref_or_literal -> Expr
+        = i:identifier { Expr::Ref(i) }
         / int_literal
+
+    identifier -> String
+        = [a-zA-Z]+ { match_str.to_owned() }
 
     int_literal -> Expr
         = [0-9]+ { Expr::Literal(match_str.to_owned()) }
 
-    _ = " "*
+    _ = [ \t]*
 "#);
 
-unsafe fn codegen(input: Expr) {
+unsafe fn codegen(input: Vec<Expr>) {
     let context = llvm::core::LLVMContextCreate();
     let module = llvm::core::LLVMModuleCreateWithName(b"example_module\0".as_ptr() as *const _);
     let builder = llvm::core::LLVMCreateBuilderInContext(context);
@@ -72,9 +92,16 @@ unsafe fn codegen(input: Expr) {
     let bb = llvm::core::LLVMAppendBasicBlockInContext(context, function, entry_name.as_ptr());
     llvm::core::LLVMPositionBuilderAtEnd(builder, bb);
 
-    // The juicy part: construct a `LLVMValue` from a Rust value:
-    let return_value = codegen_expr(context, builder, input);
+    let mut names = HashMap::new();
+    insert_allocations(context, builder, &mut names, &input);
 
+    let int_type = llvm::core::LLVMInt64TypeInContext(context);
+    let zero = llvm::core::LLVMConstInt(int_type, 0, 0);
+
+    let mut return_value = zero; // return value on empty program
+    for expr in input {
+        return_value = codegen_expr(context, builder, function, &mut names, expr);
+    }
     llvm::core::LLVMBuildRet(builder, return_value);
 
     // Instead of dumping to stdout, let's write out the IR to `out.ll`
@@ -86,9 +113,30 @@ unsafe fn codegen(input: Expr) {
     llvm::core::LLVMContextDispose(context);
 }
 
+unsafe fn insert_allocations(context: LLVMContextRef, builder: LLVMBuilderRef, names: &mut HashMap<String, LLVMValueRef>, exprs: &[Expr]) {
+    let mut variable_names = HashSet::new();
+    for expr in exprs {
+        match *expr {
+            Expr::Assign(ref name, _) => {
+                variable_names.insert(name);
+            },
+
+            _ => {},
+        }
+    }
+
+    for variable_name in variable_names {
+        let int_type = llvm::core::LLVMInt64TypeInContext(context);
+        let name = CString::new(variable_name.as_bytes()).unwrap();
+        let pointer = llvm::core::LLVMBuildAlloca(builder, int_type, name.as_ptr());
+
+        names.insert(variable_name.to_owned(), pointer);
+    }
+}
+
 // When you write out instructions in LLVM, you get back `LLVMValueRef`s. You
 // can then use these references in other instructions.
-unsafe fn codegen_expr(context: LLVMContextRef, builder: LLVMBuilderRef, expr: Expr) -> LLVMValueRef {
+unsafe fn codegen_expr(context: LLVMContextRef, builder: LLVMBuilderRef, func: LLVMValueRef, names: &mut HashMap<String, LLVMValueRef>, expr: Expr) -> LLVMValueRef {
     match expr {
         Expr::Literal(int_literal) => {
             let int_type = llvm::core::LLVMInt64TypeInContext(context);
@@ -96,35 +144,90 @@ unsafe fn codegen_expr(context: LLVMContextRef, builder: LLVMBuilderRef, expr: E
         },
 
         Expr::Add(lhs, rhs) => {
-            let lhs = codegen_expr(context, builder, *lhs);
-            let rhs = codegen_expr(context, builder, *rhs);
+            let lhs = codegen_expr(context, builder, func, names, *lhs);
+            let rhs = codegen_expr(context, builder, func, names, *rhs);
 
             let name = CString::new("addtmp").unwrap();
             llvm::core::LLVMBuildAdd(builder, lhs, rhs, name.as_ptr())
         },
 
         Expr::Sub(lhs, rhs) => {
-            let lhs = codegen_expr(context, builder, *lhs);
-            let rhs = codegen_expr(context, builder, *rhs);
+            let lhs = codegen_expr(context, builder, func, names, *lhs);
+            let rhs = codegen_expr(context, builder, func, names, *rhs);
 
             let name = CString::new("subtmp").unwrap();
             llvm::core::LLVMBuildSub(builder, lhs, rhs, name.as_ptr())
         },
 
         Expr::Mul(lhs, rhs) => {
-            let lhs = codegen_expr(context, builder, *lhs);
-            let rhs = codegen_expr(context, builder, *rhs);
+            let lhs = codegen_expr(context, builder, func, names, *lhs);
+            let rhs = codegen_expr(context, builder, func, names, *rhs);
 
             let name = CString::new("multmp").unwrap();
             llvm::core::LLVMBuildMul(builder, lhs, rhs, name.as_ptr())
         },
 
         Expr::Div(lhs, rhs) => {
-            let lhs = codegen_expr(context, builder, *lhs);
-            let rhs = codegen_expr(context, builder, *rhs);
+            let lhs = codegen_expr(context, builder, func, names, *lhs);
+            let rhs = codegen_expr(context, builder, func, names, *rhs);
 
             let name = CString::new("divtmp").unwrap();
             llvm::core::LLVMBuildUDiv(builder, lhs, rhs, name.as_ptr())
+        },
+
+        Expr::Ref(name) => {
+            let pointer = names.get(&name).unwrap();
+            let name = CString::new(name).unwrap();
+            llvm::core::LLVMBuildLoad(builder, *pointer, name.as_ptr())
+        },
+
+        Expr::Assign(name, expr) => {
+            let new_value = codegen_expr(context, builder, func, names, *expr);
+            let pointer = names.get(&name).unwrap();
+            llvm::core::LLVMBuildStore(builder, new_value, *pointer);
+            new_value
+        },
+
+        Expr::If(condition, then_body, else_body) => {
+            let condition_value = codegen_expr(context, builder, func, names, *condition);
+            let int_type = llvm::core::LLVMInt64TypeInContext(context);
+            let zero = llvm::core::LLVMConstInt(int_type, 0, 0);
+
+            let name = CString::new("is_nonzero").unwrap();
+            let is_nonzero = llvm::core::LLVMBuildICmp(builder, llvm::LLVMIntPredicate::LLVMIntNE, condition_value, zero, name.as_ptr());
+
+            let entry_name = CString::new("entry").unwrap();
+            let then_block = llvm::core::LLVMAppendBasicBlockInContext(context, func, entry_name.as_ptr());
+            let else_block = llvm::core::LLVMAppendBasicBlockInContext(context, func, entry_name.as_ptr());
+            let merge_block = llvm::core::LLVMAppendBasicBlockInContext(context, func, entry_name.as_ptr());
+
+            llvm::core::LLVMBuildCondBr(builder, is_nonzero, then_block, else_block);
+
+            llvm::core::LLVMPositionBuilderAtEnd(builder, then_block);
+            let mut then_return = zero;
+            for expr in then_body {
+                then_return = codegen_expr(context, builder, func, names, expr);
+            }
+            llvm::core::LLVMBuildBr(builder, merge_block);
+            let then_block = llvm::core::LLVMGetInsertBlock(builder);
+
+            llvm::core::LLVMPositionBuilderAtEnd(builder, else_block);
+            let mut else_return = zero;
+            for expr in else_body {
+                else_return = codegen_expr(context, builder, func, names, expr);
+            }
+            llvm::core::LLVMBuildBr(builder, merge_block);
+            let else_block = llvm::core::LLVMGetInsertBlock(builder);
+
+            llvm::core::LLVMPositionBuilderAtEnd(builder, merge_block);
+            let phi_name = CString::new("iftmp").unwrap();
+            let phi = llvm::core::LLVMBuildPhi(builder, int_type, phi_name.as_ptr());
+
+            let mut values = vec![then_return, else_return];
+            let mut blocks = vec![then_block, else_block];
+
+            llvm::core::LLVMAddIncoming(phi, values.as_mut_ptr(), blocks.as_mut_ptr(), 2);
+            phi
         },
     }
 }
